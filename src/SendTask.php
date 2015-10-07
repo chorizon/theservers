b88a16ffaf4654af2abed7102e16e1d2822e6221
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-10-07 22:17:44 +0200

    Added class for load processes

---
+++ src/SendTask.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Chorizon\TheServers;
+
+Webmodel::load_model('vendor/chorizon/theservers/models/models_servers');
+
+class SendTask {
+
+	static public function begin_task($arr_data_task)
+	{
+	
+        //Execute the daemon
+        
+        //Obtain pid from daemon
+	
+	}
+	
+	static public function get_task_progress($idtask)
+	{
+	
+        
+	
+	}
+
+}
+
+?>
